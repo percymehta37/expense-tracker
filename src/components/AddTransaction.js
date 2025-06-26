2d97e0da2effe43ce4846be164dc3c5679b7aa21
Percy Mehta <telepeirce@gmail.com>
2025-06-26 18:01:22 +0530

    Fix: typo in AddTransaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,4 +1,4 @@
-import Reac, {useState,useContext} from 'react'
+import React, {useState,useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
 const AddTransaction = () => {
     const[text,setText] = useState('');
